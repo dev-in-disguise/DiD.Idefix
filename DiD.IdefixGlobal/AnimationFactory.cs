0d378c728221ab28bffc82154676432a154b32b5
Manuel Zelenka <manuel.zelenka@gmail.com>
2021-04-04 09:18:01 +0200

    Adds initial version with just idefix being printed

---
+++ DiD.IdefixGlobal/AnimationFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace DiD.IdefixGlobal
+{
+    internal static class AnimationFactory
+    {
+        internal static void ShowIdefix()
+        {
+            var idefixAscii = File.ReadAllLines(@".\Resources\Idefix.txt");
+
+            foreach (var asciiLine in idefixAscii)
+            {
+                Console.WriteLine(asciiLine);
+            }
+        }
+    }
+}
---
+++ DiD.IdefixGlobal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiD.IdefixGlobal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimationFactory.ShowIdefix();
+        }
+    }
+}
