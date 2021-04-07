cc422ab0c9d2d04f6cbd9bed2a802296ae549c46
Manuel Zelenka <manuel.zelenka@gmail.com>
2021-04-07 06:42:41 +0200

    Prints supported commands when more than one arg is provided

--- src/DiD.Idefix/Program.cs
+++ src/DiD.Idefix/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace DiD.Idefix
+﻿using System;
+using System.Linq;
+
+namespace DiD.Idefix
 {
     class Program
     {
         static void Main(string[] args)
         {
-            AnimationFactory.ShowIdefix();
+            if (!args.Any())
+            {
+                AnimationFactory.ShowIdefix();
+                return;
+            }
+
+            if(args.Length > 1)
+            {
+                Console.WriteLine("Supported idefix commands: ");
+                Console.WriteLine(" - idefix");
+                Console.WriteLine(" - idefix wiggle");
+            }
         }
     }
 }
